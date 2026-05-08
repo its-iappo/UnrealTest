18087d7a6b50d775c3ea5a310b74c439ac24b842
Gabriele Laera <gabrilaer157@gmail.com>
2026-05-08 12:20:29 +0200

    ok i'm stupid
    i don't even remember what yawn does but setting it at 500.0f fixs camera not following where player looks

--- Source/UnrealTest/Private/CoreChar/UnrealTestCharacter.cpp
+++ Source/UnrealTest/Private/CoreChar/UnrealTestCharacter.cpp
@@ -22,7 +22,7 @@ AUnrealTestCharacter::AUnrealTestCharacter()
 
 	// Configure character movement
 	GetCharacterMovement()->bOrientRotationToMovement = true;
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 0.0f, 0.0f);
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
 
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
